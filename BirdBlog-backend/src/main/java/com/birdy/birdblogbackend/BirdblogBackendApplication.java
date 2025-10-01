f114fa7f60a4ee12eb26a98a343ec5f6dabe59d1
Birdy <2944006086@qq.com>
2025-10-01 22:13:02 +0800

    refactor: 重新构建与初始化后端环境

---
+++ BirdBlog-backend/src/main/java/com/birdy/birdblogbackend/BirdblogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.birdy.birdblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdblogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirdblogBackendApplication.class, args);
+    }
+
+}
---
+++ BirdBlog-backend/src/test/java/com/birdy/birdblogbackend/BirdblogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birdy.birdblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdblogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
