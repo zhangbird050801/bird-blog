c21f6cc22220f9b44d38708dd40e89ee536e058f
Birdy <2944006086@qq.com>
2025-10-24 17:34:45 +0800

    fix: 启动类添加包扫描，防止 bean 注入报错

--- BirdBlog-backend/BirdBlog-blog/src/main/java/com/birdy/BirdBlogApplication.java
+++ BirdBlog-backend/BirdBlog-blog/src/main/java/com/birdy/BirdBlogApplication.java
@@ -1,9 +1,11 @@
 package com.birdy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.birdy.mapper")
 public class BirdBlogApplication
 {
     public static void main( String[] args )
