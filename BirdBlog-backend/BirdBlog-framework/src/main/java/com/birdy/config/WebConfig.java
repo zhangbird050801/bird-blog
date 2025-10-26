e4936f8ab908f708b0d29bbed53cdb3039a3c72e
Birdy <2944006086@qq.com>
2025-10-26 15:00:32 +0800

    feat: 解决跨域问题

---
+++ BirdBlog-backend/BirdBlog-framework/src/main/java/com/birdy/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.birdy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Birdy
+ * @date 2025/10/26 14:54
+ * @description WebConfig
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // 解决跨域问题
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
