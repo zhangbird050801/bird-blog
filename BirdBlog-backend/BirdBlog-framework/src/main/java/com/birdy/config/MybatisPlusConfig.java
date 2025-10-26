274013e5287ee6954c3143d138bebe653296c5fa
Birdy <2944006086@qq.com>
2025-10-26 15:42:57 +0800

    fix: 修复 mybatis-plus 分页查询失效的问题 feat: MybatisPlusConfig 配置分页插件 PaginationInnerInterceptor

---
+++ BirdBlog-backend/BirdBlog-framework/src/main/java/com/birdy/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.birdy.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Birdy
+ * @date 2025/10/26 15:38
+ * @description MybatisPlusConfig
+ */
+@Configuration
+@MapperScan("com.birdy.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
