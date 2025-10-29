dc2fb6f342e089b7cf23b592da8a95b3ed4fbfde
Birdy <2944006086@qq.com>
2025-10-29 16:51:44 +0800

    refactor: 格式化articleVO的时间

--- BirdBlog-backend/BirdBlog-framework/src/main/java/com/birdy/domain/vo/ArticleVO.java
+++ BirdBlog-backend/BirdBlog-framework/src/main/java/com/birdy/domain/vo/ArticleVO.java
@@ -1,5 +1,6 @@
 package com.birdy.domain.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -53,5 +54,6 @@ public class ArticleVO {
     /**
      * 发布时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date publishedTime;
 }
