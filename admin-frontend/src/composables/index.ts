668cd73eb8410fba29d63a825f1b48ad4fc827a5
Birdy <2944006086@qq.com>
2025-10-31 17:29:00 +0800

    refactor: 不再使用 vue3-element-admin

--- admin-frontend/src/composables/index.ts
+++
@@ -1,8 +0,0 @@
-export { useStomp } from "./websocket/useStomp";
-export { useDictSync } from "./websocket/useDictSync";
-export type { DictMessage } from "./websocket/useDictSync";
-export { useOnlineCount } from "./websocket/useOnlineCount";
-
-export { useLayout } from "./layout/useLayout";
-export { useLayoutMenu } from "./layout/useLayoutMenu";
-export { useDeviceDetection } from "./layout/useDeviceDetection";
